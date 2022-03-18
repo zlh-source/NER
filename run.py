1dfe7c798c2a6f5767bb89fa771603b08492d93a
zlh <50194803+zlh-source@users.noreply.github.com>
2022-03-18 22:08:07 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,17 @@
 import argparse
-import random
-import numpy as np
+from main import train,test,get_predict
 import torch
-import os
-
-def seed_everything(seed=7):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
-seed_everything()
-
-from main import *
+from util import seed_everything
 
 parser = argparse.ArgumentParser(description='Model Controller')
-parser.add_argument('--cuda_id', default="7", type=str,help='显卡号')
-parser.add_argument('--cpu', default=True, type=bool,help='是否使用cpu?')
+parser.add_argument('--cuda_id', default="0,2,3", type=str,help='显卡号')
+parser.add_argument('--cpu', default=False, type=bool,help='是否使用cpu?')
 
 parser.add_argument('--dataset_path', default='./datasets/CLUENER', type=str, help="")
-parser.add_argument('--train', default="train", type=str,help="train or test or predict")
-parser.add_argument('--file_id', default="00", type=str)
+parser.add_argument('--train', default="test", type=str,help="train or test or predict")
+parser.add_argument('--file_id', default="99", type=str)
+
+parser.add_argument('--seed', default=0, type=int)
 
 #超参数
 #Encoder
@@ -32,7 +20,7 @@ parser.add_argument('--file_id', default="00", type=str)
 
 #loss
 parser.add_argument('--info', default='', type=str,help='关于模型的描述')
-parser.add_argument('--batch_size', default=32, type=int)
+parser.add_argument('--batch_size', default=128, type=int)
 parser.add_argument('--learning_rate', default=3e-5, type=float)
 parser.add_argument('--num_train_epochs', default=10, type=int)
 
@@ -43,22 +31,22 @@ parser.add_argument('--max_grad_norm', default=1.0, type=float)
 parser.add_argument('--min_num', default=1e-7, type=float)
 
 #不用改
-parser.add_argument('--bert_directory', default="./pretrained/base-chinese", type=str)
+parser.add_argument('--bert_directory', default="base-chinese", type=str)
 
 args = parser.parse_args()
 
 
 print('begin')
+args.cuda_id=list(map(int,args.cuda_id.split(",")))
+
 if args.cpu:
     device = "cpu"
 else:
-    try:
-        torch.cuda.set_device(int(args.cuda_id))
-    except:
-        os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_id
-    device = "cuda:"+args.cuda_id
+    device = "cuda:%d"%(args.cuda_id[0])
 
 args.device = torch.device(device)
+
+seed_everything(args.seed)
 
 if args.train=="train":
     train(args)
